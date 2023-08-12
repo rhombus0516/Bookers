0c8f79b3b0886b354641f9e2d52b2ef3df3b30aa
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-08-12 08:58:08 +0000

    [Add]showを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,22 @@
 class BooksController < ApplicationController
   def new
+     
   end
 
-  def index
-    @books =Book.new
+  def index 
+    @book =Book.new
+    @books = Book.all
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/homes/top'
+    
+    redirect_to book_path(@books.id)
   end
 
   def show
+    @books = Book.find(params[:id])
   end
 
   def edit
@@ -20,7 +24,7 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+   params.require(:book).permit(:title, :body)
   end
     
 end
